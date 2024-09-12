d8c73743120c01f04a2dc3ad0c9e44df593b722c
Michelle Madeira <michellemadeira1@gmail.com>
2024-09-12 12:48:35 -0300

    Repository

---
+++ src/main/java/com/projeto/manicure/repository/EsmalteRepository.java
@@ -0,0 +1,14 @@
+package com.projeto.manicure.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projeto.manicure.entity.Esmalte;
+
+@Repository
+public interface EsmalteRepository extends JpaRepository<Esmalte, Long> {
+ 
+	List<Esmalte> findAllByOrderByNumeroAsc();
+}
